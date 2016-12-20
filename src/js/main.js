1027852ee20e7539a1ada9547aaa128c0dce25cb
nickjevershed <nick.evershed@gmail.com>
2016-12-20 18:17:08 +1100

    Working chart

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,10 @@ import mainHTML from './text/main.html!text'
 import share from './lib/share'
 import parseURL from './lib/parseURL'
 import fetchJSON from './lib/fetch'
+import makeHeader from './makeHeader'
+import makeCharts from './makeCharts'
 import * as d3 from 'd3'
+
 
 
 var debug = true;
@@ -38,7 +41,7 @@ export function init(el, context, config, mediator) {
 
         //Make the header
 
-        // makeHeader()
+        makeHeader(debug);
 
         //Make the results charts 
 
@@ -48,161 +51,10 @@ export function init(el, context, config, mediator) {
             crossOrigin: true,
             success: function(resp) { 
                 (debug) ? console.log(resp) : null;
-                makeCharts(resp)
+                makeCharts(debug,resp)
             }
         })
 
-        // Make the tables
-
-    }
-
-    function makeCharts(chartData) {
-        makeHeader(chartData);        
-    }
-
-    function makeHeader(chartData) {
-        var width = document.querySelector("#header").getBoundingClientRect().width;
-        var height = document.querySelector("#header").getBoundingClientRect().height;
-        var data = {};    
-
-        (debug) ? console.log("width",width,"height",height) : null;
-
-        function makeData() {
-            data['red'] = []
-            data['blue'] = []
-
-            var redBase = 50;
-            var blueBase = 60;
-            var smallMove = 1;
-            var bigMove = 4;
-            var biggerMove = 5;
-
-            for (var i = 0; i < 100; i++) {
-                if (i < 60) {
-                    data['red'].push(redBase)
-                    data['blue'].push(blueBase)
-                }
-
-                else if (i <80 ) {
-
-                    var r = Math.random()
-                    if (r < 0.4) {
-                        redBase = redBase - smallMove;
-                    }
-                    if (r < 0.6) {
-                        redBase = redBase + smallMove;
-                    }
-
-                    if (r <0.8) {
-                        redBase = redBase - bigMove;
-                    }
-                    else {
-                        redBase = redBase + bigMove;
-                    }
-
-                    data['red'].push(redBase) 
-
-                    var r = Math.random()
-
-                    if (r < 0.4) {
-                        blueBase = blueBase + smallMove;
-                    }
-                    if (r < 0.6) {
-                        blueBase = blueBase - smallMove;
-                    }
-
-                    if (r <0.8) {
-                        blueBase = blueBase + bigMove;
-                    }
-                    else {
-                        blueBase = blueBase - bigMove;
-                    }
-
-                    data['blue'].push(blueBase)
-                }
-
-                else {
-
-                    var r = Math.random()
-                    if (r < 0.4) {
-                        redBase = redBase + smallMove;
-                    }
-                    if (r < 0.6) {
-                        redBase = redBase - smallMove;
-                    }
-
-                    if (r <0.8) {
-                        redBase = redBase + bigMove;
-                    }
-                    else {
-                        redBase = redBase - bigMove;
-                    }
-
-                    data['red'].push(redBase) 
-
-                    var r = Math.random()
-
-                    if (r < 0.4) {
-                        blueBase = blueBase - smallMove;
-                    }
-                    if (r < 0.6) {
-                        blueBase = blueBase + smallMove;
-                    }
-
-                    if (r <0.8) {
-                        blueBase = blueBase - bigMove;
-                    }
-                    else {
-                        blueBase = blueBase + bigMove;
-                    }
-
-                    data['blue'].push(blueBase)
-
-                }
-
-            };
-
-            console.log(data['red']);
-            console.log(data['blue']);
-        }
-
-        makeData();
-        console.log(data);
-
-        var svg = d3.select('#backgroundHeaderGraph').append('svg')
-            .attr("width", width)
-            .attr("height", height)
-
-        var x = d3.scaleLinear()
-            .range([0, width]);
-
-        var y = d3.scaleLinear()
-                .range([height, 0]);
-
-        x.domain([0,100]);
-        y.domain([-10,100]);  
-
-        var line = d3.line()
-                .x(function(d,i) { return x(i) })
-                .y(function(d,i) { return y(d) });    
-
-        svg.append("path")
-              .attr("class", "red line")
-              .style("stroke","#b51800")
-              .attr("d", line(data['red']))
-
-        svg.append("path")
-              .attr("class", "blue line")
-              .style("stroke","#005689")
-              .attr("d", line(data['blue']))     
-
-        var headerUpdateTimer = setInterval(function() {
-            makeData();
-            var t0 = svg.transition().duration(1000);
-            t0.selectAll(".red").attr("d", line(data['red']));
-            t0.selectAll(".blue").attr("d", line(data['blue']));
-
-        },2000)
 
     }
 
